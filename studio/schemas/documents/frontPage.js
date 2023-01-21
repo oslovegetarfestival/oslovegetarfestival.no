4d3b2aa5fa04211f1329d6c9ebee82aad7d36ec4
tommyno <tommy.nordum@gmail.com>
2023-01-21 19:22:50 +0100

    Add sponsors to Sanity

--- studio/schemas/documents/frontPage.js
+++ studio/schemas/documents/frontPage.js
@@ -1,11 +1,50 @@
 import { intro } from "../parts/intro";
+import { image } from "../parts/image";
 import { title } from "../parts/title";
 
 export const frontPage = {
   name: "frontPage",
   title: "Forside",
   type: "document",
-  fields: [title(), intro()],
+  fields: [
+    title(),
+    intro(),
+    {
+      title: "Liste med sponsorer",
+      name: "sponsorBlock",
+      type: "object",
+      fields: [
+        {
+          title: "Tittel (valgfritt)",
+          name: "title",
+          type: "string",
+          description: "For eksempel: Sponsorer",
+        },
+        {
+          title: "Sponsorer",
+          name: "sponsors",
+          type: "array",
+          of: [
+            {
+              name: "sponsor",
+              title: "Sponsor",
+              type: "object",
+              fields: [
+                title({ title: "Navn på sponsor" }),
+                image({ title: "Logo" }),
+                {
+                  title: "Lenke",
+                  description: "Lenke til sponsors nettside (valgfritt)",
+                  name: "url",
+                  type: "url",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
 
   preview: {
     select: {
