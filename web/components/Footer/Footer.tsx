6c084e72550871cf99d766cc1352c28523340436
elisejakob <hei@elisejakob.no>
2023-01-24 20:47:58 +0100

    add some footer content

--- web/components/Footer/Footer.tsx
+++ web/components/Footer/Footer.tsx
@@ -1,26 +1,79 @@
-import { Flex, Section } from "components/Layout"
+import Link from "next/link"
+
+import { Grid, Flex, Section } from "components/Layout"
 
 import styles from "./Footer.module.scss"
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Section width="large" verticalPadding="large">
-        <Flex justify="spaceBetween">
+      <Section width="full" verticalPadding="large">
+        <Grid>
           <div>
-            <p className="h3">Oslo Vegetarfestival</p>
             <button>Kjøp billetter</button>
           </div>
 
+          <div></div>
+
           <div>
+            <h2>Kontakt</h2>
             <p>kontakt@oslovegetarfestival.no</p>
-            <p>Telefon xxxx</p>
-            <p>Facebook, Insta etc</p>
+            <p>Facebook</p>
+            <p>Instagram</p>
+            <p>Youtube</p>
           </div>
 
           <div>
-            <p>Nyhetsbrev</p>
+            <h2>Nyhetsbrev</h2>
+            <p>Få siste nytt om plantebasert mat, konkurranser, utlodninger og mye mer!</p>
+            <label>
+              E-post
+              <input type="email" />
+            </label>
           </div>
+        </Grid>
+        <Flex justify="spaceBetween" align="end">
+          <p className="h1">
+            <Link href="/">
+              <a className={styles.logo}>
+                <span>Oslo</span> <span>vegetar</span>
+                <span>festival</span>
+              </a>
+            </Link>
+          </p>
+          <nav>
+            <ul>
+              <Flex justify="spaceBetween">
+                <li className={styles.menuItem}>
+                  <Link href="/aktuelt">Aktuelt</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/bli-frivillig">Bli frivillig</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/for-utstillere">For utstillere</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/om-oss">Om oss</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/praktisk-info">Praktisk info</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/program">Program</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/utstillere">Utstillere</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/english">English</Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href="/testside">Testside</Link>
+                </li>
+              </Flex>
+            </ul>
+          </nav>
         </Flex>
       </Section>
     </footer>
