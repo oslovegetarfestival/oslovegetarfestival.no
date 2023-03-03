bd4da4414bcd13df2b59a52c0b82040499e0d936
tommyno <tommy.nordum@gmail.com>
2023-03-03 06:38:50 +0100

    Remove legacy intro field in accordion

--- studio/schemas/parts/accordion.js
+++ studio/schemas/parts/accordion.js
@@ -29,10 +29,6 @@ export const accordion = {
               type: "string",
               validation: (Rule) => Rule.required(),
             },
-            intro({
-              title: "Tekst (OBS! denne brukes ikke lenger)",
-              required: true,
-            }),
             richTextObject,
           ],
         },
