8adac22ddf9852546018a7094296ec2b5bef29bf
tommyno <tommy.nordum@gmail.com>
2023-04-09 07:22:46 +0200

    Fix oveflow on hero image

--- web/components/FrontpageHero/FrontpageHero.tsx
+++ web/components/FrontpageHero/FrontpageHero.tsx
@@ -4,29 +4,31 @@ import styles from "./FrontpageHero.module.scss"
 
 export const FrontpageHero = () => {
   return (
-    <Section width="large" verticalPadding="large" noPadding="top">
-      <div className={styles.logo}>
-        <span>Oslo</span>
-        <span>Vegetar</span>
-        <span>festival</span>
-        <Block top="5">
-          <span className={styles.date}>Kubaparken</span>
-          <span className={styles.date}>27.-28. mai 2023</span>
-        </Block>
-      </div>
-
-      <Block top="7">
-        <Button size="large" color="red" link="/billetter">
-          Kjøp billetter
-        </Button>
-      </Block>
-
+    <div style={{ position: "relative", overflow: "hidden" }}>
       <img
         src="/frontpage-hero.svg"
         alt=""
         aria-hidden
         className={styles.heroImage}
       />
-    </Section>
+
+      <Section width="large" verticalPadding="large" noPadding="top">
+        <div className={styles.logo}>
+          <span>Oslo</span>
+          <span>Vegetar</span>
+          <span>festival</span>
+          <Block top="5">
+            <span className={styles.date}>Kubaparken</span>
+            <span className={styles.date}>27.-28. mai 2023</span>
+          </Block>
+        </div>
+
+        <Block top="7">
+          <Button size="large" color="red" link="/billetter">
+            Kjøp billetter
+          </Button>
+        </Block>
+      </Section>
+    </div>
   )
 }
