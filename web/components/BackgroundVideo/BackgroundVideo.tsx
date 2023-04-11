aba818664ba4bc0460c542ecd45ac8037c6ca909
tommyno <tommy.nordum@gmail.com>
2023-04-11 18:20:30 +0200

    Make frontpage video autoplay and loop

--- web/components/BackgroundVideo/BackgroundVideo.tsx
+++ web/components/BackgroundVideo/BackgroundVideo.tsx
@@ -1,21 +1,10 @@
 import { Section } from "components/Layout"
 import { Video } from "components/Video"
-import styles from "./BackgroundVideo.module.scss"
 
 export const BackgroundVideo = () => {
   return (
-    <Section
-      centerContent
-      width="full"
-      verticalPadding="large"
-      background="kale"
-    >
-      <Section centerContent width="large" noPadding="sides">
-        <Video id="AeV9j9EJzMA" />
-        {/* <video className={styles.wrap} autoPlay muted loop>
-        <source src="/vegetarfestival-promo.mp4" type="video/mp4" />
-      </video> */}
-      </Section>
+    <Section width="large" noPadding="sides">
+      <Video id="rUthsu7i7v8" isAutoplay />
     </Section>
   )
 }
--- web/components/Video/Video.tsx
+++ web/components/Video/Video.tsx
@@ -2,14 +2,19 @@ import styles from "./Video.module.scss"
 
 type Props = {
   id: string
+  isAutoplay?: boolean
 }
 
-export const Video = ({ id }: Props) => {
+export const Video = ({ id, isAutoplay = false }: Props) => {
   return (
     <div className={styles.wrap}>
+      {/* Note: Autoplay will only work if video is by default set to mute */}
+      {/* Note: Loop only works if we've defined a playlist */}
       <iframe
         className={styles.video}
-        src={`https://www.youtube-nocookie.com/embed/${id}?rel=0`}
+        src={`https://www.youtube-nocookie.com/embed/${id}?rel=0${
+          isAutoplay ? `&autoplay=1&mute=1&loop=1&playlist=${id}` : ""
+        }`}
         title="YouTube video player"
         allow="encrypted-media"
         allowFullScreen
