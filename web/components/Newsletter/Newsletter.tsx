3a2a23dc0261df74ff0b8e857aa2718515f219b9
tommyno <tommy.nordum@gmail.com>
2023-04-11 18:37:48 +0200

    Add link to newsletter signup in zoho

--- web/components/Newsletter/Newsletter.tsx
+++ web/components/Newsletter/Newsletter.tsx
@@ -4,20 +4,28 @@ import { Button } from "components/Button"
 
 export const Newsletter = () => {
   return (
-    <form>
-      <label>
-        <Flow space="small">
-          <div>
-            <strong>E-post</strong>
-          </div>
-          <input type="email" className={styles.input} required />
-          <div>
-            <Button color="white" isArrow={false} type="submit">
-              Meld deg på
-            </Button>
-          </div>
-        </Flow>
-      </label>
-    </form>
+    <Button
+      link="https://zcmp.eu/sZa"
+      color="white"
+      size="small"
+      isOpenInNewWindow
+    >
+      Meld deg på nyhetsbrevet
+    </Button>
+    // <form>
+    //   <label>
+    //     <Flow space="small">
+    //       <div>
+    //         <strong>E-post</strong>
+    //       </div>
+    //       <input type="email" className={styles.input} required />
+    //       <div>
+    //         <Button color="white" isArrow={false} type="submit">
+    //           Meld deg på
+    //         </Button>
+    //       </div>
+    //     </Flow>
+    //   </label>
+    // </form>
   )
 }
