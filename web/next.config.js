5da37344c94ad8810e22461a29ab19f09786bbdb
tommyno <tommy.nordum@gmail.com>
2023-04-11 18:33:05 +0200

    Remember scroll position on back/forward

--- web/next.config.js
+++ web/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "cdn.sanity.io" }],
   },
+  experimental: {
+    scrollRestoration: true,
+  },
   async redirects() {
     return [
       {
