46e9821ff40cd01e06df65eb1277f44efbbb4639
tommyno <tommy.nordum@gmail.com>
2023-04-12 22:03:34 +0200

    Add video test page to debug youtube

---
+++ web/pages/videotest.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from "next"
+
+import { PageLayout } from "components/PageLayout"
+import { Flow, Section } from "components/Layout"
+import { Video } from "components/Video"
+
+type Props = {
+  data: Record<string, unknown>
+}
+
+const Home: NextPage<Props> = () => {
+  return (
+    <>
+      <PageLayout>
+        <Section verticalPadding="xlarge">
+          <Flow>
+            <h2>#1 video component</h2>
+            <Video id="rUthsu7i7v8" />
+
+            <h2>#2 video component other</h2>
+            <Video id="BHRyfEbhFFU" />
+
+            <h2>#3 video raw embed</h2>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/rUthsu7i7v8"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+
+            <h2>#4 video privacy</h2>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/rUthsu7i7v8"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </Flow>
+        </Section>
+      </PageLayout>
+    </>
+  )
+}
+
+export default Home
