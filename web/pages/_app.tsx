f130a1ccd148ce4f8132010f43997d72320a5561
tommyno <tommy.nordum@gmail.com>
2023-04-17 19:21:56 +0200

    Add new favicons

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -58,13 +58,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           sizes="16x16"
           href="/favicon/favicon-16x16.png"
         />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       {/* https://nextjs.org/docs/basic-features/script */}
