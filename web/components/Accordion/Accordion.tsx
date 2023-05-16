3cb3832dc3eba3ef32e085af95ec13b1751ba4bd
tommyno <tommy.nordum@gmail.com>
2023-05-16 19:30:47 +0200

    Make accordion more colorful

--- web/components/Accordion/Accordion.tsx
+++ web/components/Accordion/Accordion.tsx
@@ -19,34 +19,32 @@ export const Accordion = ({ title, list }: Props) => {
       {title && <h2>{title}</h2>}
 
       <div className={styles.wrap}>
-        <Flow space="small">
-          {list?.map(({ title, richTextObject, _key }) => (
-            <details key={_key} className={styles.details}>
-              <summary className={styles.summary}>
-                <Flex align="center" justify="spaceBetween" gap="small">
-                  <h3 className="h5">
-                    <strong>{title}</strong>
-                  </h3>
+        {list?.map(({ title, richTextObject, _key }) => (
+          <details key={_key} className={styles.details}>
+            <summary className={styles.summary}>
+              <Flex align="center" justify="spaceBetween" gap="small">
+                <h3 className="h5">
+                  <strong>{title}</strong>
+                </h3>
 
-                  {/* Icon from https://reactsvgicons.com */}
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    height="24px"
-                    width="24px"
-                    className={styles.icon}
-                  >
-                    <path d="M12 6.879l-7.061 7.06 2.122 2.122L12 11.121l4.939 4.94 2.122-2.122z" />
-                  </svg>
-                </Flex>
-              </summary>
+                {/* Icon from https://reactsvgicons.com */}
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  height="24px"
+                  width="24px"
+                  className={styles.icon}
+                >
+                  <path d="M12 6.879l-7.061 7.06 2.122 2.122L12 11.121l4.939 4.94 2.122-2.122z" />
+                </svg>
+              </Flex>
+            </summary>
 
-              <div className={styles.content}>
-                <RichText data={richTextObject?.richText} />
-              </div>
-            </details>
-          ))}
-        </Flow>
+            <div className={styles.content}>
+              <RichText data={richTextObject?.richText} />
+            </div>
+          </details>
+        ))}
       </div>
     </Flow>
   )
