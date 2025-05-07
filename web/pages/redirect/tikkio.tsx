eaedf20819618b5720514bcef40855f7a4254053
Tommy Nordum <tommy@spindelvev.net>
2025-05-07 07:04:52 +0200

    Add some basic info to Tikkio redirect page

--- web/pages/redirect/tikkio.tsx
+++ web/pages/redirect/tikkio.tsx
@@ -1,3 +1,6 @@
+import { Button } from "components/Button"
+import { Flow, Section } from "components/Layout"
+import { PageLayout } from "components/PageLayout"
 import type { NextPage } from "next"
 import { useEffect } from "react"
 
@@ -13,7 +16,18 @@ const TikkioRedirect: NextPage = () => {
     return () => clearTimeout(redirectTimer)
   }, [])
 
-  return null
+  return (
+    <PageLayout>
+      <Section width="small" noPadding="bottom">
+        <Flow>
+          <h3>Du sendes nå videre til vår billettleverandør Tikkio.</h3>
+          <Button link="https://tikkio.com/events/48169-oslo-vegetarfestival-2025">
+            Kjøp billett
+          </Button>
+        </Flow>
+      </Section>
+    </PageLayout>
+  )
 }
 
 export default TikkioRedirect
