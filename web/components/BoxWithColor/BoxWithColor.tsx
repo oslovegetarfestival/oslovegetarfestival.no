4d7b4e96432eec25828f8cc5486ea97b8fc1d7fc
Tommy Nordum <tommyno@users.noreply.github.com>
2026-03-04 13:46:53 +0100

    Update all deps to latest major (#9)
    * Fix: handle undefined page in ExhibitorPage component
    
    * Update web and sanity deps to latest major

--- web/components/BoxWithColor/BoxWithColor.tsx
+++ web/components/BoxWithColor/BoxWithColor.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import styles from "./BoxWithColor.module.scss"
 
 type Props = {
-  children: React.ReactNode | JSX.Element[] | JSX.Element
+  children: React.ReactNode
 }
 
 export const BoxWithColor: React.FC<Props> = ({ children }) => {
